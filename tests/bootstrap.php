87944c0751eeff8fccce32a879985b23dced11f1
anton <kornilov@realweb.ru>
2014-11-19 17:02:11 +0300

    fix bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 /* @var Composer\Autoload\ClassLoader $autoloader */
 $autoloader = include __DIR__ . '/../vendor/autoload.php';
-$autoloader->addPsr4('Kachit\Codegenerator', __DIR__ . '/../src');
-$autoloader->addPsr4('Kachit\Codegenerator\Test', __DIR__);
+$autoloader->add('Kachit\Codegenerator\Test', __DIR__);
 
 $class = new Kachit\Codegenerator\Entity\Classes();
