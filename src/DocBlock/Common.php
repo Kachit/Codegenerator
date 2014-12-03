c83b7c687b04c195b9fbb9e2339c6ce4edf0964c
anton <kornilov@realweb.ru>
2014-12-03 15:20:49 +0300

    fix bootstrap

--- src/DocBlock/Common.php
+++ src/DocBlock/Common.php
@@ -9,10 +9,21 @@ namespace Kachit\Codegenerator\DocBlock;
 
 use Kachit\Codegenerator\Entity\Collection;
 
-class Common {
+class Common implements DocBlockInterface {
 
+    /**
+     * @var string
+     */
     protected $title;
+
+    /**
+     * @var string
+     */
     protected $description;
+
+    /**
+     * @var Collection
+     */
     protected $tags;
 
     /**
@@ -20,5 +31,65 @@ class Common {
      */
     public function __construct() {
         $this->tags = new Collection();
+    }
+
+    /**
+     * Add tag
+     *
+     * @param Tag $tag
+     * @return $this
+     */
+    public function addTag(Tag $tag) {
+        $this->tags->add($tag);
+        return $this;
+    }
+
+    /**
+     * Get Description
+     *
+     * @return string
+     */
+    public function getDescription() {
+        return $this->description;
+    }
+
+    /**
+     * Set Description
+     *
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription($description) {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get Tags
+     *
+     * @return Collection
+     */
+    public function getTags() {
+        return $this->tags;
+    }
+
+    /**
+     * Get Title
+     *
+     * @return string
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * Set Title
+     *
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
     }
 } 
