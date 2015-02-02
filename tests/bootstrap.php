2efc957e3a947c128e6f7f41d2f018d6f6d444e3
Kachit <antoxachaos@gmail.com>
2015-02-03 00:29:11 +0300

    initial commit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,3 @@
 /* @var Composer\Autoload\ClassLoader $autoloader */
 $autoloader = include __DIR__ . '/../vendor/autoload.php';
 $autoloader->add('Kachit\Codegenerator\Test', __DIR__);
-
-$class = new Kachit\Codegenerator\Entity\Classes();
