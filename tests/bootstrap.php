6f1965320b4f841c5caf6e748ae98f6bee13ab64
Kachit <antoxachaos@gmail.com>
2015-02-08 19:12:18 +0300

    initial commit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 /* @var Composer\Autoload\ClassLoader $autoloader */
-$autoloader = include __DIR__ . '/../vendor/autoload.php';
-$autoloader->add('Kachit\Codegenerator\Test', __DIR__);
+$autoloader = require_once __DIR__ . '/../vendor/autoload.php';
+$autoloader->addPsr4('Kachit\Codegenerator\Test\\', __DIR__);
