bb41e53507c8133c8e5d6f187ce4ee8609ba07b4
Antoxa <kornilov@realweb.ru>
2015-02-10 17:27:44 +0300

    refactor to psr-4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 /* @var Composer\Autoload\ClassLoader $autoloader */
 $autoloader = require_once __DIR__ . '/../vendor/autoload.php';
-$autoloader->add('Kachit\\Codegenerator\\Test\\', __DIR__);
+$autoloader->addPsr4('Kachit\\Codegenerator\\Tests\\', __DIR__);
